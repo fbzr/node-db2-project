0c1d34f7afcad5ba5d28783f5768ca18d1936d6f
fbzr <fabricio.spb@gmail.com>
2020-04-13 07:26:45 -0700

    create dbConfig

--- routes/api/cars.js
+++ routes/api/cars.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// @desc    Return all cars
 router.get('/', (req, res) => res.send('cars route'));
 
 module.exports = router;
